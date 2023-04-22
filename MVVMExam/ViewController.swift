c3e59012facbb407989da3ab84843aa03f03b4b8
Arpitthummar <113656339+Arpitthummar@users.noreply.github.com>
2023-04-22 08:15:33 +0530

    Initial Commit

---
+++ MVVMExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExam
+//
+//  Created by Arpit Thummar on 22/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVMExamUITests/MVVMExamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVMExamUITestsLaunchTests.swift
+//  MVVMExamUITests
+//
+//  Created by Arpit Thummar on 22/04/23.
+//
+
+import XCTest
+
+final class MVVMExamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
